978ebcb2749f98472b1b9e124f8813548b6924df
27771 <27771>
2022-12-19 13:35:44 +0800

    Base Window & Base Struct

---
+++ OpenFrp.Launcher/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿
+global using Frame = ModernWpf.Controls.Frame;
+global using Page = ModernWpf.Controls.Page;
+global using ModernWpf.Controls;
+global using ModernWpf;
+global using System.Windows.Media.Animation;
+global using System.Windows;
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None,
+    ResourceDictionaryLocation.SourceAssembly
+)]
